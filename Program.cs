7338887ea7e9fceb63274a1f36677e2d29f3fb5b
D1zarm <featweath1337@gmail.com>
2022-11-29 20:26:06 +0300

    Выполнил решение задачи и добавил git ignore

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿string[] arr1 = new string[5] { "Hello", "2", "world", ":-)", "-2" };
+string[] arr2 = new string[arr1.Length];
+void Find(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        if (arr1[i].Length <= 3)
+        {
+            arr2[count] = arr1[i];
+            count++;
+        }
+    }
+}
+void Printarr(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+Find(arr1, arr2);
+Printarr(arr2);
